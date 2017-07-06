d7b2aa11fa5e7ed1a8bfcfbe0662162022fe0b9c
freizeitnerd <freizeitnerd@googlemail.com>
2017-07-06 22:47:49 +0200

    restructure dic

--- root/sprinkler/main.py
+++ root/sprinkler/main.py
@@ -1,26 +1,6 @@
 #!/usr/bin/python
-import valve
-import onionGpio
+from valve import Valve
 import time
-    
-class Valve:
-  totalCount = 0
-
-  def __init__(self, name, gpioNumber):
-    print 'Init valve "' + name + '" at GPIO', gpioNumber
-    self.gpioNumber = gpioNumber
-    self.name = name
-    self.gpio = onionGpio.OnionGpio(gpioNumber)
-    self.status = self.gpio.setOutputDirection(1)
-    Valve.totalCount += 1
- 
-  def open(self):
-    print 'open', self.name, "at GPIO "+self.
-    self.gpio.setOutputDirection(0)
-  
-  def close(self):
-    print 'close', self.name
-    self.gpio.setOutputDirection(1)
 
 valves = []
 valveNames = ['rear1', 'lawn1', 'lawn2', 'front', 'front2', 'rear2', 'tapWater']
---
+++ root/sprinkler/valve.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import onionGpio
+
+class Valve:
+  totalCount = 0
+
+  def __init__(self, name, gpioNumber):
+    print 'Init valve "' + name + '" at GPIO', gpioNumber
+    self.gpioNumber = gpioNumber
+    self.name = name
+    self.gpio = onionGpio.OnionGpio(gpioNumber)
+    self.status = self.gpio.setOutputDirection(1)
+    Valve.totalCount += 1
+
+  def open(self):
+    print 'open', self.name, "at GPIO",self.gpioNumber
+    self.gpio.setOutputDirection(0)
+
+  def close(self):
+    print 'close', self.name
+    self.gpio.setOutputDirection(1)
